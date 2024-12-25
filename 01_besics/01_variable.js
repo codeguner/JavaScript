8a6859492e4f8b95aab96ef2ba50e995c1641da6
Rahul Naskar <naskarrahul483@gmail.com>
2024-12-25 08:16:42 +0000

    code added

---
+++ 01_besics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "rahul@gmail.com"
+var accountPassword = "2345820"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 87633  not allowed
+accountEmail = "som@gmail.com"
+accountPassword = "48738493"
+accountCity = "Patna"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
