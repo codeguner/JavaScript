793c0d6ff50604e7bcfd9a5fbe564f2e1613cb67
Rahul Naskar <naskarrahul483@gmail.com>
2024-12-26 06:44:31 +0000

    code added

--- 01_besics/01_variable.js
+++ 01_besics/01_variable.js
@@ -4,6 +4,12 @@ var accountPassword = "2345820"
 accountCity = "Jaipur"
 let accountState;
 
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
 // accountId = 87633  not allowed
 accountEmail = "som@gmail.com"
 accountPassword = "48738493"
